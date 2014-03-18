1b743891ed1927dbb15e6587dd4b7df1f2d2fa03
Miguel Palhas <mpalhas@gmail.com>
2014-03-18 17:32:57 +0000

    Using turnservers.com API

--- js/go-rtc-turn.js
+++ js/go-rtc-turn.js
@@ -8,69 +8,74 @@ $(function() {
   var url = 'https://goinstant.net/dd8e6d3f8389/webRTC_tests'
   var $container = $('#container');
 
-  goinstant.connect(url, function(err, platform, room) {
-    if (err) {
-      throw err;
-    }
-
-    var turnServer = {
-      url: 'turn:yniche.com:3478',
-      username: 'yniche_test',
-      credential: 'yniche_test'
-    };
-
-    var webrtc = new goinstant.widgets.WebRTC({
-      room: room,
-      expandContainer: $container.find('#webinar-expand')[0],
-      listContainer: $container.find('#webinar-webrtc')[0],
-      peerConnectionConfig: {
-        iceServers: [ turnServer ]
+  window.turnserversDotComAPI.iceServers(function(iceData) {
+    goinstant.connect(url, function(err, platform, room) {
+      if (err) {
+        throw err;
       }
+
+      // var stunServer = { url: 'stun:stun.l.google.com:19302' };
+      // var turnServer = {
+      //   url: 'turn:yniche.com:3478',
+      //   username: 'yniche_test',
+      //   credential: 'yniche_test'
+      // };
+
+      debugger
+      var webrtc = new goinstant.widgets.WebRTC({
+        room: room,
+        expandContainer: $container.find('#webinar-expand')[0],
+        listContainer: $container.find('#webinar-webrtc')[0],
+        peerConnectionConfig: {
+          iceServers: iceData // [ stunServer, turnServer ]
+        }
+      });
+
+      var userList = new goinstant.widgets.UserList({
+        room: room,
+        collapsed: true,
+        container: $container.find('#webinar-userlist')[0]
+      });
+
+      var chat = new goinstant.widgets.Chat({
+        room: room,
+        position: 'left'
+      });
+
+      var notifications = new goinstant.widgets.Notifications({
+        container: $container.find('#webinar-notifications')[0]
+      });
+
+      webrtc.initialize(genericErrorHandler);
+      userList.initialize(genericErrorHandler);
+      chat.initialize(genericErrorHandler);
+
+      notifications.subscribe(room, genericErrorHandler);
+
+      var baderousMsg = {
+        room: room,
+        type: 'success',
+        message: 'Baderous!',
+        displayToSelf: true
+      };
+
+      var johnnyMsg = {
+        room: room,
+        type: 'error',
+        message: 'O joão é larilas',
+        displayToSelf: true
+      };
+
+      $('.baderous').on('click', function() {
+        console.log('baderous');
+        notifications.publish(baderousMsg, genericErrorHandler);
+      });
+
+      $('.johnny').on('click', function() {
+        console.log('johnny');
+        notifications.publish(johnnyMsg, genericErrorHandler);
+      });
     });
 
-    var userList = new goinstant.widgets.UserList({
-      room: room,
-      collapsed: true,
-      container: $container.find('#webinar-userlist')[0]
-    });
-
-    var chat = new goinstant.widgets.Chat({
-      room: room,
-      position: 'left'
-    });
-
-    var notifications = new goinstant.widgets.Notifications({
-      container: $container.find('#webinar-notifications')[0]
-    });
-
-    webrtc.initialize(genericErrorHandler);
-    userList.initialize(genericErrorHandler);
-    chat.initialize(genericErrorHandler);
-
-    notifications.subscribe(room, genericErrorHandler);
-
-    var baderousMsg = {
-      room: room,
-      type: 'success',
-      message: 'Baderous!',
-      displayToSelf: true
-    };
-
-    var johnnyMsg = {
-      room: room,
-      type: 'error',
-      message: 'O joão é larilas',
-      displayToSelf: true
-    };
-
-    $('.baderous').on('click', function() {
-      console.log('baderous');
-      notifications.publish(baderousMsg, genericErrorHandler);
-    });
-
-    $('.johnny').on('click', function() {
-      console.log('johnny');
-      notifications.publish(johnnyMsg, genericErrorHandler);
-    });
   });
 });
